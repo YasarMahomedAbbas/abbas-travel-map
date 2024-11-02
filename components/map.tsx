1ebd5629588aa6dede87f9f77b2152f698f828ef
Yasar Abbas <yasar.tm44@gmail.com>
2024-11-02 22:46:02 +0100

    fixed funciton error

--- components/map.tsx
+++ components/map.tsx
@@ -7,7 +7,7 @@ import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Fix for default markers - with proper typing
-delete (L.Icon.Default.prototype as { _getIconUrl?: Function })._getIconUrl;
+delete (L.Icon.Default.prototype as { _getIconUrl?: () => string })._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
